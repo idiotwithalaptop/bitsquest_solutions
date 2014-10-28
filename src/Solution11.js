292134844248b2e90065d6fbc9b951534c4e82af
Ryan Brown <piston47@gmail.com>
2014-10-28 13:36:11 +1100

    Added completed solution for challenge 11

--- src/Solution11.js
+++ src/Solution11.js
@@ -5,46 +5,84 @@
  */
 
 var start = [
-  {position: 38, thruster: 'left', angle: 0},
-  {position: 22, thruster: 'bottom', angle: 270},
+  {position: 48, thruster: 'left', angle: 0},
+  {position: 28, thruster: 'bottom', angle: 270},
   {position: 28, thruster: 'left', angle: 0}
 ]
 
 var phases = [
-  {position: 38, thruster: 'left', angle: 0},
-  {position: 22, thruster: 'bottom', angle: 270},
-  {position: 28, thruster: 'left', angle: 0},
-  {position: 5, thruster: 'bottom', angle: 270},
-  {position: 18, thruster: 'left', angle: 0},
-  {position: 28, thruster: 'top', angle: 90},
-  {position: 28, thruster: 'right', angle: 180},
-  {position: 1, thruster: 'top', angle: 90}
+  [
+    {position: 18, thruster: 'left', angle: 0},
+    {position: 13, thruster: 'bottom', angle: 270},
+    {position: 28, thruster: 'top', angle: 90},
+    {position: 28, thruster: 'right', angle: 180},
+  ],
+  [
+    {position: 13, thruster: 'bottom', angle: 270},
+    {position: 13, thruster: 'left', angle: 0},
+    {position: 28, thruster: 'top', angle: 90},
+    {position: 28, thruster: 'right', angle: 180},
+  ],
+  [
+    {position: 18, thruster: 'left', angle: 0},
+    {position: 13, thruster: 'bottom', angle: 270},
+    {position: 28, thruster: 'top', angle: 90},
+    {position: 28, thruster: 'right', angle: 180},
+  ],
+  [
+    {position: 18, thruster: 'right', angle: 180},
+    {position: 13, thruster: 'bottom', angle: 270},
+    {position: 13, thruster: 'left', angle: 0},
+    {position: 28, thruster: 'top', angle: 90},
+    {position: 28, thruster: 'right', angle: 180},
+  ],
+  [
+    {position: 18, thruster: 'left', angle: 0},
+    {position: 13, thruster: 'bottom', angle: 270},
+    {position: 28, thruster: 'top', angle: 90},
+    {position: 28, thruster: 'right', angle: 180},
+  ],
+  [
+    {position: 13, thruster: 'bottom', angle: 270},
+    {position: 13, thruster: 'left', angle: 0},
+    {position: 28, thruster: 'top', angle: 90},
+    {position: 28, thruster: 'right', angle: 180},
+  ],
+  [
+    {position: 18, thruster: 'left', angle: 0},
+    {position: 13, thruster: 'bottom', angle: 270},
+    {position: 28, thruster: 'top', angle: 90},
+    {position: 28, thruster: 'right', angle: 180},
+  ],
 ];
-var currentPhase = 0;
+
+var steps = [];
+
+var phaseIndex = -1;
+var currentStep = 0;
+var currentPhase = null;
 var initialised = false;
 var ratio = 0;
+var checking = false;
 
 var resetRight = 30;
 var resetTop = 24;
 
 this.on('start', function() {
-  this.radar.angle(0);
+  this.radar.angle(180);
   this.radar.ping();
 });
 
 this.on('radar:hit', function(angle, distance) {
-  if(initialised) {
-  }
-  else {
-    initialised = true;
-    ratio = distance;
-    console.log(ratio);
-    this.radar.angle(0);
-    this.thrusters.left(true);
-    
-  }
-    /*
-    var phase = phases[currentPhase];
+  if(checking) {
+	steps = phases[++phaseIndex];
+    currentStep = 0;
+    checking = false;
+    this.radar.angle(steps[0].angle);
+    this.thrusters[steps[0].thruster](true);
+  } 
+  else if(initialised) {
+    var phase = steps[currentStep];
     console.log(distance + ' < ' + (phase.position * ratio));
     if(distance < (phase.position * ratio)) {
       this.thrusters.left(false);
@@ -52,9 +90,16 @@ this.on('radar:hit', function(angle, distance) {
       this.thrusters.right(false);
       this.thrusters.bottom(false);
       
-      phase = phases[++currentPhase];
-      this.radar.angle(phase.angle);
-      this.thrusters[phase.thruster](true);
+      phase = steps[++currentStep];
+      if(typeof phase === 'undefined') {
+        checking = true;
+        this.radar.angle(90);
+      } else {
+      
+      	this.radar.angle(phase.angle);
+      	this.thrusters[phase.thruster](true);
+      }
+      
     }
   }
   else {
@@ -63,11 +108,12 @@ this.on('radar:hit', function(angle, distance) {
     console.log(ratio);
     this.radar.angle(0);
     this.thrusters.left(true);
+    steps = start;
+    
   }
   this.radar.ping();
-*/
 });
 
-this.reset = function() {
-  console.log('RESETING');
-}
+this.on('radar:miss', function(angle, distance) {
+  this.thrusters.top(true);
+});
